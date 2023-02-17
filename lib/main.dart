df0658f50d7b73b03dbffabce1d573aa56565d07
Gaël Thomas <sainibhimsingh2001@gmail.com>
2023-02-17 08:34:49 +0530

    remove imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'Screens/Dashboard/Myntra_Project/Dashboard/29_Chat_Intake.dart';
-import 'Screens/Dashboard/Myntra_Project/Dashboard/30_Chat.dart';
-import 'Screens/Dashboard/Myntra_Project/Dashboard/33_Astro_Live.dart';
-import 'Screens/Dashboard/Myntra_Project/Dashboard/38_Recharge_History.dart';
-import 'Screens/Dashboard/Myntra_Project/Dashboard/7_Home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
